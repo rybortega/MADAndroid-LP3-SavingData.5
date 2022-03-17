6d5eb855268b5e1737fea276692f59015fd0c2f3
Stacey Lai <staceyclai@gmail.com>
2022-03-16 21:00:00 -0700

    Added functionality to add a new card or cancel

---
+++ app/src/main/java/com/example/staceysflashcardapp/AddCardActivity.java
@@ -0,0 +1,37 @@
+package com.example.staceysflashcardapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class AddCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_card);
+
+        findViewById(R.id.flashcard_cancel_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        findViewById(R.id.flashcard_save_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent data = new Intent();
+                String inputQuestion = ((EditText) findViewById(R.id.flashcard_question_edit_text)).getText().toString();
+                String inputAnswer = ((EditText) findViewById(R.id.flashcard_answer_edit_text)).getText().toString();
+                data.putExtra("QUESTION_KEY", inputQuestion);
+                data.putExtra("ANSWER_KEY", inputAnswer);
+                setResult(RESULT_OK, data);
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/staceysflashcardapp/MainActivity.java
+++ app/src/main/java/com/example/staceysflashcardapp/MainActivity.java
@@ -1,23 +1,61 @@
 package com.example.staceysflashcardapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView questionTextView;
+    TextView answerTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        questionTextView = findViewById(R.id.flashcard_question);
+        answerTextView = findViewById(R.id.flashcard_answer);
+
         findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
-                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+                questionTextView.setVisibility(View.INVISIBLE);
+                answerTextView.setVisibility(View.VISIBLE);
             }
         });
+
+        ImageView addQuestionImageView = findViewById(R.id.flashcard_add_question_button);
+        addQuestionImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                //startActivity(intent);
+                startActivityForResult(intent, 100);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        questionTextView = findViewById(R.id.flashcard_question);
+        answerTextView = findViewById(R.id.flashcard_answer);
+
+        if (requestCode == 100){
+            //get data
+            if (data != null){ //check if there's an Intent
+                String questionString = data.getExtras().getString("QUESTION_KEY");
+                String answerString = data.getExtras().getString("ANSWER_KEY");
+                questionTextView.setText(questionString);
+                answerTextView.setText(answerString);
+            }
+        }
     }
 }
