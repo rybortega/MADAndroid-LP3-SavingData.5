a2bec1e5251f06eeb23801359c3c3b89a61bf43f
Stacey Lai <staceyclai@gmail.com>
2022-03-19 11:30:34 -0700

    Added functionality to go to next card and save cards in app

--- app/src/main/java/com/example/staceysflashcardapp/MainActivity.java
+++ app/src/main/java/com/example/staceysflashcardapp/MainActivity.java
@@ -9,15 +9,30 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView questionTextView;
     TextView answerTextView;
+    FlashcardDatabase flashcardDatabase;
+    List<Flashcard> allFlashcards;
+    int currentCardDisplayedIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        flashcardDatabase = new FlashcardDatabase(getApplicationContext());
+        allFlashcards = flashcardDatabase.getAllCards();
+
+        if (allFlashcards != null && allFlashcards.size() > 0) {
+            ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(0).getQuestion());
+            ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(0).getAnswer());
+        }
 
         questionTextView = findViewById(R.id.flashcard_question);
         answerTextView = findViewById(R.id.flashcard_answer);
@@ -29,6 +44,13 @@ public class MainActivity extends AppCompatActivity {
                 answerTextView.setVisibility(View.VISIBLE);
             }
         });
+        findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                questionTextView.setVisibility(View.VISIBLE);
+                answerTextView.setVisibility(View.INVISIBLE);
+            }
+        });
 
         ImageView addQuestionImageView = findViewById(R.id.flashcard_add_question_button);
         addQuestionImageView.setOnClickListener(new View.OnClickListener() {
@@ -37,6 +59,37 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
                 //startActivity(intent);
                 startActivityForResult(intent, 100);
+            }
+        });
+
+        findViewById(R.id.flashcard_next_question_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // don't try to go to next card if you have no cards to begin with
+                if (allFlashcards.size() == 0)
+                    return;
+                // advance our pointer index so we can show the next card
+                currentCardDisplayedIndex++;
+                // set view to question automatically, if not on the answer
+                questionTextView.setVisibility(View.INVISIBLE);
+                answerTextView.setVisibility(View.VISIBLE);
+
+                // make sure we don't get an IndexOutOfBoundsError if we are viewing the last indexed card in our list
+                if(currentCardDisplayedIndex >= allFlashcards.size()) {
+                    Snackbar.make(questionTextView,
+                            "You've reached the end of the cards, going back to start.",
+                            Snackbar.LENGTH_SHORT)
+                            .show();
+                    currentCardDisplayedIndex = 0;
+                }
+
+                // set the question and answer TextViews with data from the database
+                allFlashcards = flashcardDatabase.getAllCards();
+                Flashcard flashcard = allFlashcards.get(currentCardDisplayedIndex);
+
+                ((TextView) findViewById(R.id.flashcard_question)).setText(flashcard.getAnswer());
+                ((TextView) findViewById(R.id.flashcard_answer)).setText(flashcard.getQuestion());
             }
         });
     }
@@ -55,7 +108,12 @@ public class MainActivity extends AppCompatActivity {
                 String answerString = data.getExtras().getString("ANSWER_KEY");
                 questionTextView.setText(questionString);
                 answerTextView.setText(answerString);
+
+                flashcardDatabase.insertCard(new Flashcard(questionString, answerString));
+                allFlashcards = flashcardDatabase.getAllCards();
             }
         }
+
+
     }
 }
